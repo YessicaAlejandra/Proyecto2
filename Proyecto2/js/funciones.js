1dbc42b439df8f0f18248c9efd5702360b19df36
Yessica Hernández Pérez <89716197+YessicaAlejandra@users.noreply.github.com>
2021-09-21 17:57:49 -0500

    Add files via upload

---
+++ Proyecto2/js/funciones.js
@@ -0,0 +1,9 @@
+$(document).ready(function){
+    $(window).scroll(function){
+        if( $(this).scrollTop() > 0)}
+        $('header').addClass('header');
+    }else{
+        $('header').removeClass('header');
+    }
+    });
+});
